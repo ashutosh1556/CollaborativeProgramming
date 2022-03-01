f82d0eb809f6fe6a3f1c4e04c120a35eb925537b
Ashutosh Verma <ashutver@amazon.com>
2022-03-01 10:46:58 +0530

    Added two String programs and restructured directories

--- src/com/pratice/programming/india/datastructures/linkedlist/LinkedList.java
+++ src/com/pratice/programming/india/datastructures/linkedlist/LinkedList.java
@@ -1,4 +1,4 @@
-package com.pratice.programming.india.datastructures;
+package com.pratice.programming.india.datastructures.linkedlist;
 
 class Node {
     int data;
---
+++ src/com/pratice/programming/india/string/StringMethods.java
@@ -0,0 +1,14 @@
+package com.pratice.programming.india.string;
+
+import java.util.LinkedList;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String str = "java";
+        String newString = str.replace('a','b');
+        char charAtIndex = str.charAt(0);
+        char[] charArray = str.toCharArray();
+        int indexOfLetter = str.indexOf('j');
+        String subString = str.substring(2,4);
+    }
+}
