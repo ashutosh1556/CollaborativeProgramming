b7a06f560401ed27b1230d8d1dfe3b6a3810578e
RJ0209 <69031570+RJ0209@users.noreply.github.com>
2022-03-07 19:02:14 -0800

    new file added

--- src/com/pratice/programming/canada/strings/IsAnagram.java
+++ src/com/pratice/programming/canada/strings/IsAnagram.java
@@ -6,13 +6,14 @@ public class IsAnagram {
 
 
     public static void main(String[] args) {
-      String str1= "ruchiabc";
-String str2 = "acbruchi";
-IsAnagram anagram = new IsAnagram();
-        System.out.println(anagram.isAnagram(str1, str2));}
-    boolean isAnagram(String str1, String str2) {
+        String str1 = "ruchi";
+        String str2 = "Ruchi";
+        IsAnagram anagram = new IsAnagram();
+        System.out.println(anagram.isAnagram(str1, str2));
+    }
 
-        boolean isAnagram = false;
+    boolean isAnagram(String str1, String str2) {
+        boolean isAnagram = true;
 
         if (str1 == null || str2 == null) return false;
         else if (str1.length() != str2.length()) return false;
@@ -23,15 +24,19 @@ IsAnagram anagram = new IsAnagram();
             char[] str2Arr = str2.toCharArray();
             Arrays.sort(str1Arr);
             Arrays.sort(str2Arr);
-
             System.out.println(str1Arr);
             System.out.println(str2Arr);
-           /*(str1Arr == str2Arr) {
-                isAnagram = true;
-            }*/
+            for(int i=0; i<str1.length();i++){
+                if(str1Arr[i] != str2Arr[i])
+                {
+                    isAnagram = false;
+                    break;
+                }
+            }
         }
         return isAnagram;
     }
 }
+
 
 
